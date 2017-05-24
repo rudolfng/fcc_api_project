a4d354637ece2aa5a3303de122ba5a3410dfe8b0
Rudolf Niedhammer <rudolfng@users.noreply.github.com>
2017-05-24 11:15:50 -0400

    port changed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express()
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 app.get('/',(req,res)=> res.send('Hello world'))
 app.listen(PORT,()=>console.log(`Listening on port ${PORT}`))
