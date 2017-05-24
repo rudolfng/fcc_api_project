199adac2277a88f552eacb06b7f0533b147428af
Rudolf Niedhammer <rudolfng@users.noreply.github.com>
2017-05-24 11:17:07 -0400

    port 8080

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express()
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 app.get('/',(req,res)=> res.send('Hello world'))
 app.listen(PORT,()=>console.log(`Listening on port ${PORT}`))
